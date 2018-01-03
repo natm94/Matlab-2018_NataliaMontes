33bfc5f41b45cf2a712f05ad3e0252fb0e048c67
natm94 <34948893+natm94@users.noreply.github.com>
2018-01-03 12:24:21 -0800

    Add files via upload

---
+++ breakfastcereal.m
@@ -0,0 +1,42 @@
+%BreakfastCereal
+%
+%Provides examples of fun things you can do with cereal boxes.
+%Inspired by my husband mis-spelt childhood 
+%
+%written IF 1/3/2018
+
+frosted='FROSTED FLAKES';
+cheerios='CHEERIOS';
+
+frosted_scrambled = frosted; 
+
+frosted_scrambled(9)='E';
+
+honey_cheerios = cheerios;
+
+honey_cheerios(1)='O'; 
+
+honey_cheerios(5)='P';
+
+chem = 'CHEMISTRY';
+
+new_chem = chem;
+
+new_chem(1)='O';
+
+new_chem(8)='B';
+
+disp(new_chem);
+
+macncheese = 'MACARONI AND CHEESE'; 
+
+new_macncheese = macncheese;
+
+new_macncheese(3)='O'; 
+
+new_macncheese(14)='O';
+
+new_macncheese(8)='R'; 
+
+disp(new_macncheese); 
+
