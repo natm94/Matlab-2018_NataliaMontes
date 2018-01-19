1a08f4da9a7511c88d687c60528127f910816260
Unknown <natm94@uw.edu>
2018-01-19 10:54:09 -0800

    Finished homework. One section of unclarity at the end.

--- Homework_2_NataliaMontes.m
+++ Homework_2_NataliaMontes.m
@@ -21,7 +21,11 @@ str2(19) = 'p';
 disp(str2)
 
 % c) Now try to do this for both a) and b) in a single line of code.
-% Not sure what this means or how to go about doing this *****
+
+%single line for a
+str1(3:11)='LACK OF P' 
+%single line for b
+str2(6:19)='rouping with p'
  
 %% Q 2.2: More replacing letters in strings.
 %Starting with the string:
@@ -59,41 +63,55 @@ C = linspace(20, 1, 20)
 str = 'xxxxxxxxxxxxxxxxxxxx';
 
 % a) Use indexing to make the 2, 5, 8th and so on ... letters in the string "z" and the 3rd, 6th and 9th and so on ... letter "p".  I.e. "xzpxzpxzpxzpxzpxzpxz"
+str(2:3:end) = 'z';
+str(3:3:end) = 'p';
 
 % b) make the 5th , 6th  and 7th letters ‘r’.
+str(5:7)='r';
 
 % c) Demonstrate that the eight letter in the string is a 'z'
+disp(str(8))
 
 % d) Flip str so it is backwards (‘zxpzxpzxpzxpzrrrxpzx’). Hint (use 2.3c)
+% not sure what that hint means so I did this:
+reverse = str(end:-1:1);
 
 % e) Demonstrate that the 6th, 13th and 17th letters in the reversed string are ‘pzx’.
+disp(reverse([6 13 17]))
 
 %% Q 2.5: Indexing into vectors
  
 % You ran an experiment where you took 20 measurements every 2.36 seconds starting 1.2 seconds into the experiment.
 % a) Create a vector that describes the moments in time that these measurements were taken.
-
+measurement=(1.2:2.36:60);
+measurement=measurement(1:20);
 % b) When was the fifth measurement taken?
-
-% c) Display the last measurement, using the command 
-% end
-
+measurement(5)
+% c) Display the last measurement, using the end command
+disp(measurement(end))
 %% Q 2.6: Still more indexing
 
 % Imagine you are running an experiment where 16 stimuli flash onto the screen. Subjects are asked to hit the 'h' key if they see a happy face and a ‘s’ of they see a sad face.
 % The sad faces were presented on trials: 1     2     6     7    10    13    14    15
 % Start with stimlist='hhhhhhhhhhhhhhhh'
 % a) Turn stimlist into a string showing which trials contained happy faces and which trials contained sad faces.
-
+stimulist = 'hhhhhhhhhhhhhhhh';
+stimulist([1 2 6 7 10 13 14 15]) = 's';
 % During trials 1-4 and 9-12 sad music was playing. During the other trials happy music was playing. 
 % b) Create two vectors, sadvect and happyvect that contain the trials when sad and happy music was playing, respectively. 
-
+sadvect=([1:4 9:12]);
+happyvect=([5:8 13:16]);
 % At the end of the experiment, you end up with a string showing how the subject categorized the faces.
 
-resplist='ssshhhshsshhsssh';
-
-% c) Create strings stimlisthappy and resplisthappy that contain the values in stimlist and resplist when happy music was playing
-
+resplist='ssshhhshsshhsssh'; 
+% I WAS VERY CONFUSED AT THIS PART -- I FEEL AS THOUGH I DIDN'T UNDERSTAND
+% THE DIRECTIONS WELL
+% c) Create strings stimlisthappy and resplisthappy that contain the 
+% values in stimulist and resplist when happy music was playing
+stimlisthappy = stimulist([5:8 13:16]);
+resplisthappy = resplist([5:8 13:16]);
 % d) Find out which stimulus was being presented on the 3rd happy music trial.
-
+stimlisthappy(3)
 % e) Find out which responses were made on the 3rd, 4th and 5th sad music trial.
+resplistsad=resplist([1:4 9:12]);
+resplistsad([3 4 5])
