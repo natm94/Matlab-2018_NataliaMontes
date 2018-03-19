4d50534c5d1b0a16014c4a0f4bf88474156f9e49
Natalia Montes <natm94@uw.edu>
2018-03-18 23:26:26 -0400

    so many errors

--- PlotFunkyData_NataliaMontes.m
+++ PlotFunkyData_NataliaMontes.m
@@ -57,21 +57,22 @@ sterr=sqrt([[n_women * women_pc * women_apple]./n_women ...
 
 a2 = subplot(2,2,2); title('bar plot with errorbars');
 
-p2 = barweb([women_pc women_apple; men_pc men_apple], sterr, .5, {'women' 'men'}, 'gender & computers', [],'percent',[ 1 .5 .5; 1 0 1]); 
+p2 = barweb([women_pc women_apple; men_pc men_apple], sterr, .5, {'women' 'men'}, 'gender & computers','percent',[ 1 .5 .5; 1 0 1]); 
+%KEEP GETTING AN ERROR ABOVE
 
 set(a2, 'YLim', [0 1.2])
 
 % ================================%
 % QUESTION 1. Add a stacked bar plot in this subplot. 
 % use >doc bar for help or try this code as an example
-y = [1 2 3; 4 5 6];
-ax1 = subplot(2,1,1);
-bar(ax1,y)
-ax2 = subplot(2,1,2);
-bar(ax2,y,'stacked')
 
-a3=subplot(2,2,3)
+bar(subplot(2,1,1), f)
+    set(gca,'XTickLabel',{'Apple','PC'});
+    legend({'Female','Male'})
 
+bar(subplot(2,1,2), f, 'stacked')
+    set(gca,'XTickLabel',{'Apple','PC'});
+    legend({'Female','Male'})
 
 
 % ================================% 
@@ -86,6 +87,11 @@ p5 = pie([ men_pc men_apple]); hold on
 
 %% do computers rot your brain
 
+p.friends = [3 5 9 20 49 16 13 4 6 19 5 0];
+p.hours_gaming = [3 5 19 50 20 13 49 1 4 6 24 2];
+p.hours_slept = [ 8 9 7 4 5 4 3 9 10 8 7 10];
+p.Netflix = [ 1 7 2 8 3 9 4 19 39 1 2 4];
+
 facebook_header=find(strcmp(headers,'friends'));
 grade_header=find(strcmp(headers,'Exam1'));
 
@@ -96,22 +102,58 @@ plot([data{:,facebook_header}], [data{:, grade_header}], 'ko');
 xlabel('Facebook friends')
 ylabel('grade')
 
+
 % ================================% 
 % QUESTION 2.add a subplot that is the same as the one above but
 % replaces facebook friends with the number of gaming hours 
 % ================================% 
+
+set(subplot(2,2,1), 'XLim',[-500, 3500])
+set(subplot(2,2,1), 'YLim',[30, 110])
+set(subplot(2,2,1), 'XTick', [0 1500 3000])
+set(subplot(2,2,1), 'YTick', 30:10:100)
+
+subplot(2,2,2)
+plot(p.friends, p.hours_gaming)
+xlabel('Facebook friends')
+ylabel('grade')
+set(subplot(2,2,2), 'XLim',[-500, 3500])
+set(subplot(2,2,2), 'YLim',[-1, 8])
+set(subplot(2,2,2), 'XTick',0:1500:3000)
+set(subplot(2,2,2), 'YTick',0:1:7)
+
 % ================================% 
 % QUESTION 3.add a subplot that is the same as the one above but
 % plots  the number of gaming hours on the x-axis, and the number of hours
 % of sleep on the y-axis
 % ================================% 
+
+subplot(2,2,3)
+plot(p.hours_gaming, p.hours_slept);
+xlabel('hours gaming')
+ylabel('hours slept')
+set(subplot(2,2,3), 'XLim',[-2.5, 12.5])
+set(subplot(2,2,3), 'YLim',[3, 11])
+set(subplot(2,2,3), 'XTick',0:5:10)
+set(subplot(2,2,3), 'YTick',4:1:10)
+
 % ================================% 
-% QUESTION 4. In figure 3. use plot3d to do a 3d plot comparing sleep, gaming and
+% QUESTION 4. In figure 3. use plot3d to do a 3d plot comparing hours slept, gaming and
 % facebookfriends
 % ================================% 
+
+subplot(2,2,4)
+plot(p.hours_gaming, p.Netflix);
+xlabel('hours gaming')
+ylabel('Netflix')
+set(subplot(2,2,4), 'XLim',[-2.5, 12.5])
+set(subplot(2,2,4), 'YLim',[0, 5])
+set(subplot(2,2,4), 'XTick',0:5:10)
+set(subplot(2,2,4), 'YTick',1:1:4)
+
 % ================================% 
 % QUESTION 5. Choose anything to plot and plot it
 % ================================% 
 
-
-
+figure(3)
+scatter(p.hours_gaming, p.hours_slept)
